e4e1fbcba64a7d46bbd3e885fe5fcfdb2614f933
Madi Abio <madelineabio@gmail.com>
2024-08-27 11:11:05 -0600

    Finished tests for the most part

--- ps1/FormulaTests/FormulaSyntaxTests.cs
+++ ps1/FormulaTests/FormulaSyntaxTests.cs
@@ -6,7 +6,7 @@
 
 namespace CS3500.FormulaTests;
 
-using CS3500.Formula1; // Change this using statement to use different formula implementations.
+using CS3500.Formula3; // Change this using statement to use different formula implementations.
 
 /// <summary>
 ///   <para>
@@ -115,21 +115,32 @@ public class FormulaSyntaxTests
         _ = new Formula("1E1");
     }
 
-
-
     /// <summary>
     /// <para>
     ///     This test checks to see if the correct kind of exception is thrown for
-    ///     an incorrectly defined variable, also has added benefit of checking that 
-    ///     scientific notation isn't working because of incorrect variable definition 
-    ///     (ie, 1e1 works, but 1a1 should not also work).
+    ///     an incorrectly defined variable (just the letter 'e' out of context has
+    ///     no meaning)
     /// </para>
     /// </summary>
     [TestMethod]
     [ExpectedException(typeof(FormulaFormatException))]
     public void FormulaConstructor_TestValidTokenVariable_Invalid()
     {
-        _ = new Formula("1a1");
+        _ = new Formula("e");
+    }
+
+    /// <summary>
+    /// <para>
+    ///     This test checks to see if the correct kind of exception is thrown for
+    ///     an incorrectly defined variable (just the letter 'E' out of context has
+    ///     no meaning)
+    /// </para>
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(FormulaFormatException))]
+    public void FormulaConstructor_TestValidTokenVariableUpper_Invalid()
+    {
+        _ = new Formula("E");
     }
 
 
@@ -137,40 +148,59 @@ public class FormulaSyntaxTests
     /// <para>
     ///     This test checks that if correctly defined variables are used, no exceptions are thrown
     /// </para>
+    /// 
+    /// <remarks>
+    ///     This test also makes sure that both upper and lowercase variables work.
+    /// </remarks>
     /// </summary>
     [TestMethod]
     public void FormulaConstructor_TestValidTokenVariable_Valid()
     {
-        _ = new Formula("abc1+xyz1");
+        _ = new Formula("abc1+XYZ1");
     }
+
+    /// <summary>
+    ///     <para>
+    ///         This checks that floating point numbers are accepted as valid.
+    ///     </para>
+    /// </summary>
+    [TestMethod]
+    public void FormulaConstructor_TestValidTokenFloat_Valid()
+    {
+        _ = new Formula("1.1");
+    }
+
 
 
     // --- Tests for Closing Parenthesis Rule
 
     /// <summary>
     ///     <para>
-    ///         The number of closing parenthesis seen so far when read from left to right
-    ///         should always be greater than or equal to the number of opening parenthesis 
-    ///         seen so far.
-    ///     </para>
-    ///     <remarks>
     ///         This test checks that the correct kind of exception is thrown when reading
     ///         tokens from left to right, the number of closing parenthesis seen so far is
-    ///         NOT less than than the number of opening parenthesis seen so far.
-    ///     </remarks>
+    ///         NOT less than than the number of opening parenthesis seen so far, when the
+    ///         parenthesis are unbalanced (there's an unequal number of open & closed parenthesis)
+    ///     </para>
     /// </summary>
     [TestMethod]
     [ExpectedException(typeof(FormulaFormatException))]
-    public void FormulaConstructor_TestClosingParentheses_Invalid()
+    public void FormulaConstructor_TestClosingParenthesesWithUnbalancedParenthesis_Invalid()
     {
         _ = new Formula("1+1)");
     }
 
+    /// <summary>
+    ///     <para>
+    ///        This checks that even if there are balanced parenthesis, if the number of
+    ///        closing parentesis seen so far is not <= num opening parenthesis seen so far,
+    ///        that the correct kind of exception is thrown.
+    ///     </para>
+    /// </summary>
     [TestMethod]
     [ExpectedException(typeof(FormulaFormatException))]
     public void FormulaConstructor_TestClosingParenthesesWithBalancedParenthesis_Invalid()
     {
-        _ = new Formula("(1)+1)");
+        _ = new Formula(")1+1(");
     }
 
 
@@ -195,7 +225,7 @@ public class FormulaSyntaxTests
     ///         parenthesis are unbalanced (total num open parenthese > total num closed).
     ///     </para>
     ///     <remarks>
-    ///         Only need to check (1+1, not ((1+1), because if (1+1) is valid as above, 
+    ///         Only need to check (1+1, not ((1+1), because if (1+1) is valid as above,
     ///         then ( (1+1) is redundant.
     ///     </remarks>
     /// </summary>
@@ -206,6 +236,37 @@ public class FormulaSyntaxTests
         _ = new Formula("(1+1");
     }
 
+
+    // --- Tests for  Last Token Rule ---
+
+    /// <summary>
+    ///     <para>
+    ///         This test checks that the correct kind of exception is thrown when
+    ///         the last token of an expression is not a number, variable, or closing parenthesis.
+    ///     </para>
+    ///     <remarks>
+    ///          Ie, '1+' is invalid, because '+' is not a number, variable, or closing parenthesis.
+    ///     </remarks>
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(FormulaFormatException))]
+    public void FormulaConstructor_TestLastTokenOperator_Invalid()
+    {
+        _ = new Formula("1+");
+    }
+
+    /// <summary>
+    ///     <para>
+    ///         This checks that the correct kind of exception is thrown when there is an
+    ///         opeartor before the last token, but the last token is invalid.
+    ///     </para>
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(FormulaFormatException))]
+    public void FormulaConstructor_TestLastTokenCharacter_Invalid()
+    {
+        _ = new Formula("1+x");
+    }
 
 
     // --- Tests for First Token Rule
@@ -240,35 +301,16 @@ public class FormulaSyntaxTests
     }
 
 
-    // --- Tests for  Last Token Rule ---
-
     /// <summary>
-    ///     <para>
-    ///         This test checks that the correct kind of exception is thrown when
-    ///         the last token of an expression is not a number, variable, or closing parenthesis.
-    ///     </para>
-    ///     <remarks>
-    ///          Ie, '1+' is invalid, because '+' is not a number, variable, or closing parenthesis.
-    ///     </remarks>
+    /// <para>
+    ///     This tests checks that scientific notation 1E-1 works, even though
+    ///     it could be confused for being an invalid first character.
+    /// </para>
     /// </summary>
     [TestMethod]
-    [ExpectedException(typeof(FormulaFormatException))]
-    public void FormulaConstructor_TestLastTokenNumber_Invalid()
+    public void FormulaConstructor_TestFirstTokenScientificNotation_Valid()
     {
-        _ = new Formula("1+");
-    }
-
-    /// <summary>
-    ///     <para>
-    ///         This checks that the correct kind of exception is thrown when there is an
-    ///         opeartor before the last token, but the last token is invalid.
-    ///     </para>
-    /// </summary>
-    [TestMethod]
-    [ExpectedException(typeof(FormulaFormatException))]
-    public void FormulaConstructor_TestLastTokenNumber_Valid()
-    {
-        _ = new Formula("1+()");
+        _ = new Formula("1E-1+1");
     }
 
 
@@ -276,40 +318,84 @@ public class FormulaSyntaxTests
     /// <summary>
     ///     <para>
     ///         This test validates the rule that ONLY numbers, variables, or an open
-    ///         parenthesis maybe immediately following an open parenthesis or operator.
+    ///         parenthesis maybe immediately following an operator.
     ///     </para>
     ///     <remarks>
     ///         It does so by checking that the correct kind of exception is thrown when
-    ///         an invalid token follows an operator or open parenthesis. '1++1' is invalid
+    ///         an invalid token follows an operator. '1++1' is invalid
     ///         because '+' is an operator.
     ///     </remarks>
     /// </summary>
     [TestMethod]
     [ExpectedException(typeof(FormulaFormatException))]
-    public void FormulaConstructor_TestParenthesisOperatorFollowing_Invalid()
+    public void FormulaConstructor_TestOperatorFollowing_Invalid()
     {
         _ = new Formula("1++1");
     }
 
-    // --- Tests for Extra Following Rule ---
     /// <summary>
     ///     <para>
-    ///         Tests that ONLY an operator or a closing parenthesis can immediately follow a
-    ///         number, variable or closing parenthesis.
+    ///         This test validates the rule that ONLY numbers, variables, or an open
+    ///         parenthesis maybe immediately following an open parenthesis.
     ///     </para>
     ///     <remarks>
-    ///         This test is performed by checking the expected exception is given when an open
-    ///         parenthesis follows a number, variable or closing parenthesis.
-    ///         
+    ///         It does so by checking that the correct kind of exception is thrown when
+    ///         an invalid token follows an operator or open parenthesis. '()' is invalid
+    ///         because ')' is not a number, variable, or open parenthesis.
+    ///     </remarks>
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(FormulaFormatException))]
+    public void FormulaConstructor_TestParenthesisFollowing_Invalid()
+    {
+        _ = new Formula("()");
+    }
+
+    // --- Tests for Extra Following Rule ---
+
+    /// <summary>
+    ///     <para>
+    ///         This test checks that the expected exception is given when an open
+    ///         parenthesis follows a number.
+    ///     </para>
+    ///     <remarks>
     ///         This rule also means that implicit multiplication is invalid, because (1)(1) isn't valid.
     ///     </remarks>
     /// </summary>
     [TestMethod]
     [ExpectedException(typeof(FormulaFormatException))]
-    public void FormulaConstructor_TestExtraFollowing_Invalid()
+    public void FormulaConstructor_TestExtraFollowingParenthesis_Invalid()
     {
         _ = new Formula("1(1)");
     }
 
+    /// <summary>
+    ///     <para>
+    ///         This test checks that the expected exception is given when a
+    ///         number follows a number (seperated by a space)
+    ///     </para>
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(FormulaFormatException))]
+    public void FormulaConstructor_TestExtraFollowingNumber_Invalid()
+    {
+        _ = new Formula("1 1");
+    }
+
+
+    /// <summary>
+    /// <para>
+    ///     This test checks to see if the correct kind of exception is thrown for
+    ///     a number following a variable, also has added benefit of checking that 
+    ///     scientific notation isn't working because of incorrect variable definition 
+    ///     (ie, 1e1 works, but 1a1 should not also work).
+    /// </para>
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(FormulaFormatException))]
+    public void FormulaConstructor_TestExtraFollowingVariable_Invalid()
+    {
+        _ = new Formula("1a1");
+    }
 
 }
